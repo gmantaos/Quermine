66c6f9df83ca2f1f0a1c5425fe9618e3398e3f6d
Haath <gmantaos@gmail.com>
2018-05-23 17:43:34 +0300

    Implement SELECT query syntax

--- Quermine.SqlServer/Formatting/SqlServerQueryBuilder.cs
+++ Quermine.SqlServer/Formatting/SqlServerQueryBuilder.cs
@@ -6,5 +6,48 @@ namespace Quermine.SqlServer
 {
     internal class SqlServerQueryBuilder : QueryBuilder
     {
-    }
+		public override string SelectQuery(SelectQuery query)
+		{
+			StringBuilder str = new StringBuilder();
+			str.Append("SELECT ");
+
+			// TOP
+			if (query.limit > 0)
+			{
+				str.AppendFormat("TOP {0} ", query.limit);
+			}
+
+			str.Append(query.selectClause.ColumnSequence);
+			str.Append("\n");
+
+			str.Append(query.fromClause.ToString());
+			str.Append("\n");
+
+			str.Append(query.ConditionalQueryPart());
+
+			return str.ToString();
+		}
+
+		public override string ConditionalQueryPart(ConditionalQuery cond)
+		{
+			StringBuilder query = new StringBuilder();
+
+			// WHERE
+			if (cond.whereClause != null)
+			{
+				query.Append("WHERE ");
+				query.Append(cond.whereClause.ToString());
+				query.Append("\n");
+			}
+
+			// ORDER BY
+			if (cond.orderByClause != null)
+			{
+				query.Append(cond.orderByClause.ToString());
+				query.Append("\n");
+			}
+
+			return query.ToString();
+		}
+	}
 }
